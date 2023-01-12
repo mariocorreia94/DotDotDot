7e6c9319206a52450c43d38df2b1c9a228f95e1e
Mário Correia <mariocorreia.mc@gmail.com>
2023-01-12 17:26:27 +0000

    First Commit with VSC

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [clickedPositions, setClickedPositions] = useState([]);
+  const [removedPositions, setRemovedPositions] = useState([]);
+
+  function onClickPage(e) {
+    setClickedPositions([
+      ...clickedPositions,
+      { posX: e.pageX, posY: e.pageY },
+    ]);
+  }
+
+  function handleUndoClick() {
+    if (clickedPositions.length == 0) {
+      return;
+    }
+
+    const newCirclesArray = [...clickedPositions];
+    const popped = newCirclesArray.pop();
+
+    setClickedPositions(newCirclesArray);
+    setRemovedPositions([...removedPositions, popped]);
+  }
+
+  function handleRedoClick() {}
+
+  return (
+    <>
+      <button onClick={handleUndoClick}>Undo</button>
+      <button onClick={handleRedoClick}>Redo</button>
+      <div className="App" onClick={onClickPage}>
+        {clickedPositions.map((clickedPosition, idx) => (
+          <div
+            key={idx}
+            className="circle"
+            style={{ left: clickedPosition.posX, top: clickedPosition.posY }}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
